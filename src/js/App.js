075f5d04c2dfd3fc8c3e667cd37fcc97fd4aa542
Leon Ng <noleonah@hotmail.com>
2020-06-04 21:15:31 +0800

    fixed dragging map

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Draggable from "react-draggable";
 import "../css/main.css"
 
 class App extends React.Component{
@@ -9,58 +10,17 @@ class App extends React.Component{
 
     loadMapImg(){
         return(
-            <div id={"map"} >
+            <Draggable draggable={false} id={"map"} >
             <img id={"map-content"} src={this.filepath} />
-            </div>
+            </Draggable>
         )
-    }
-
-    dragElement(elmnt) {
-        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        if (document.getElementById(elmnt.id + "header")) {
-            // if present, the header is where you move the DIV from:
-            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-        } else {
-            // otherwise, move the DIV from anywhere inside the DIV:
-            elmnt.onmousedown = dragMouseDown;
-        }
-
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        }
-
-        function closeDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
     }
 
     render(){
         return(
         <div className={"canvas"}>
             {this.loadMapImg()}
-            {console.log(document.getElementById("map"))}
+            {/*{document.onload = ()=>{this.dragElement(document.getElementById("map"))}}*/}
         </div>
         )
     }
